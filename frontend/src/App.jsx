c07f6e2a3fea050ee802e91f182cb2a647c7e85b
Yugal Kaushik <yugalkaushik14@gmail.com>
2024-05-20 23:16:42 +0530

    fixing issues

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom';
 import Signup from './components/Signup';
 import Signin from './components/Signin';
 import Transfer from './components/Transfer';
@@ -8,11 +8,12 @@ const App = () => {
   return (
     <Router>
       <div className="App">
-        <Switch>
-          <Route path="/signup" component={Signup} />
-          <Route path="/signin" component={Signin} />
-          <Route path="/transfer" component={Transfer} />
-        </Switch>
+        <Routes>
+          <Route path='/' element={<Navigate to="/signup" replace />} />
+          <Route path='/signup' element={<Signup/>} />
+          <Route path='/signin' element={<Signin/>} />
+          <Route path='/transfer' element={<Transfer/>} />
+        </Routes>
       </div>
     </Router>
   );
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App'; // Assuming App component is imported from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
